0e6877f6ea3648c05904f5797e5aed668abf0bca
Interval0043 <williamlcoleman3@gmail.com>
2025-03-18 13:54:40 -0400

    fixes + additions
    fixes:
    * fixed the .json so it isn't bad
    * developed the function
    additions:
    * regex shenanigans
    * more variables

--- APCSP/APCSP/Program.cs
+++ APCSP/APCSP/Program.cs
@@ -4,22 +4,23 @@ namespace APCSP
 {
     public class JSON
     {
-        public required int id { get; set; }
-        public required string PeriodNumber { get; set; }
-        public required string Name { get; set; }
-        public required string Symbol { get; set; }
-        public required int AtomicNumber { get; set; }
-        public required double AtomicWeight { get; set; }
-        public required float MeltingPoint { get; set; }
-        public required float BoilingPoint { get; set; }
-        public required string Density { get; set; }
-        public required string PaRT { get; set; }
-        public required string AtomClass { get; set; }
-        public required string Group { get; set; }
-        public required string Uses { get; set; }
+        public required string[] id { get; set; }
+        public required string[] PeriodNumber { get; set; }
+        public required string[] Name { get; set; }
+        public required string[] Symbol { get; set; }
+        public required string[] AtomicNumber { get; set; }
+        public required string[] AtomicWeight { get; set; }
+        public required string[] MeltingPoint { get; set; }
+        public required string[] BoilingPoint { get; set; }
+        public required string[] Density { get; set; }
+        public required string[] PaRT { get; set; }
+        public required string[] AtomClass { get; set; }
+        public required string[] Group { get; set; }
+        public required string[] Uses { get; set; }
     }
     public class Program
     {
+        static string[] qualifierStrings = new string[3];
         public static void Main(string[] args)
         {
             //Counting / Toggle Variables
@@ -28,13 +29,14 @@ namespace APCSP
             //Storage
             string search = "";
             string[] filteredList;
-            int[] idList;
+            
+            /*int[] idList;
             string[] classificationList;
             string[] partList;
-            string[] groupList;
-            
+            string[] groupList; 
+            string[] finalList;*/
             //JSON Setup
-            using StreamReader reader = new("csvjson.json");
+            using StreamReader reader = new("final_transformed_csvjson.json");
             string JSONString = reader.ReadToEnd();
             try
             {
@@ -55,18 +57,58 @@ namespace APCSP
             
             static void FilterList(string input)
             {
-                string[] list;
-                string pattern = ",";
-                if (Regex.IsMatch(input, pattern))
+
+                string[] patternList = { ",", "[0-4]a[c-h][j-l][q-s][w-x]z", "[0-4]", "qwer", "asdfghjklzxc" };
+                if (Regex.IsMatch(input, patternList[0]))
                 {
                     string[] filterBy = input.Split(',');
                     //This is here to make sure the format is correct.
-                    foreach (string qualifier in filterBy)
+                    for (var i = 0; i < filterBy.Length; i++)
                     {
-                        if (qualifier.Length != 1)
+                        if (filterBy[i].Length != 1 || !Regex.IsMatch(filterBy[i], patternList[1], RegexOptions.IgnoreCase))
                         {
                             Console.WriteLine("Ensure the format is correct, then try again.");
                             HaltForInput();
+                            return;
+                        }
+
+                        if (Regex.IsMatch(filterBy[i], patternList[2]))
+                        {
+                            qualifierStrings[i] = filterBy[i] switch
+                            {
+                                "1" => "Metal",
+                                "2" => "Non-Metal",
+                                "3" => "Semi-metal",
+                                "4" => "Unknown"
+                            };
+                        }
+                        else if (Regex.IsMatch(filterBy[i], patternList[3]))
+                        {
+                            qualifierStrings[i] = filterBy[i].ToLower() switch
+                            {
+                                "q" => "Solid",
+                                "w" => "Liquid",
+                                "e" => "Gas",
+                                "r" => "Expected to be Solid"
+                            };
+                        }
+                        else if (Regex.IsMatch(filterBy[i], patternList[4]))
+                        {
+                            qualifierStrings[i] = filterBy[i].ToLower() switch
+                            {
+                                "a" => "No Definitive",
+                                "s" => "Alkali Metals",
+                                "d" => "Alkali Earth Metals",
+                                "f" => "Metalloids",
+                                "g" => "Poor Metals",
+                                "h" => "Transformation Metals",
+                                "j" => "Rare Earth Metals",
+                                "k" => "Halogens",
+                                "l" => "Noble Gases",
+                                "z" => "Actinide Metals",
+                                "x" => "Superheavy Elements",
+                                "c" => "Non-Metal"
+                            };
                         }
                     }
                 }
@@ -89,15 +131,19 @@ namespace APCSP
                                   "1) Metal || Q) Solid || A) No Definitive\n" +
                                   "2) Non-Metal || W) Liquid || S) Alkali Metals \n" +
                                   "3) Semi-Metal || E) Gas || D) Alkali Earth Metals\n" +
+                                  "4) Unknown || R) Expected Solid || F) Metalloids\n" +
                                   "Below is just the Group.\n" +
-                                  "F) Metalloids\n" +
                                   "G) Poor Metals\n" +
                                   "H) Transformation Metals\n" +
-                                  "J) Halogens\n" +
-                                  "K) Noble Gases");
+                                  "J) Rare Earth Metals\n" +
+                                  "K) Halogens\n" +
+                                  "L) Noble Gases\n" +
+                                  "Z) Actinide Metals\n" +
+                                  "X) Superheavy Elements\n" +
+                                  "C) Non-Metal");
                 search = Console.ReadLine();
                 FilterList(search);
-            } while (i == 0);
+            } while (i == 1);
         }
     }
 }
--- APCSP/APCSP/obj/Debug/net9.0/APCSP.AssemblyInfo.cs
+++ APCSP/APCSP/obj/Debug/net9.0/APCSP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APCSP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40e1dc6f8927d6ac3b4a10f79bcdaa85f5ee2346")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60ae09b13819c112f6d75c5eb8d3b2433ec29db9")]
 [assembly: System.Reflection.AssemblyProductAttribute("APCSP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APCSP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
